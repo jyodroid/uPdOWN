55d361b0966ea5029723a48544a25cbd7c72e6b6
jyodroid <jyodroid@gmail.com>
2013-02-28 20:14:19 -0500

    Montaje 2

--- trunk/conection.php
+++
@@ -1,7 +0,0 @@
-<?php
-$conection = mysqli_connect("localhost","jtangar1","maycry");
-if (!$conection) {
-	throw new Exception("No se conectó a DB".mysqli_errno($conection).":".mysqli_error($conection), 4) ;
-}
-mysqli_select_db($conection, 'jtangar1_updown');
-?>
